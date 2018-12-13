518da8c11033206aff2508a3003000825dfd2500
Bryan Kroger <krogebry@krogebry-dev-0.7.0>
2018-12-13 22:49:37 +0000

    SHAT-59  Codifying stack types.

--- routes/stack.rb
+++ routes/stack.rb
@@ -4,6 +4,27 @@ get '/stacks' do
 end
 
 get '/stack/create' do
-  erb 'stack/create'.to_sym
+  stacks = []
+
+  exclude_stacks = %i(Network Bastion ServiceGroup GenericASG)
+
+  Shatterdome::Stacks.constants.select{|c|
+    c unless c.match(/Exception/)
+  }.select{|c|
+    c unless exclude_stacks.include?(c)
+  }.each do |stack_class_name|
+    begin
+      desc = Shatterdome::Stacks.const_get(stack_class_name)::DESCRIPTION
+    rescue NameError
+      desc = "No description"
+    end
+
+    stacks.push({
+        description: desc,
+        stack_class_name: stack_class_name
+    })
+  end
+
+  erb 'stack/create'.to_sym, { locals: { stacks: stacks } }
 end
 
